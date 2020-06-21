83e17b068d62716ec560f52989f5b7fd84ff32ad
Chengcheng_Xiao <iconxicon@me.com>
2020-06-21 08:32:11 +0100

    Formula cleanup.

--- togl.rb
+++ togl.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Togl < Formula
   desc ""
   homepage ""
@@ -27,18 +24,5 @@ class Togl < Formula
                           "--exec_prefix=#{prefix}"
     system "make"
     system "make", "install"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Togl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
